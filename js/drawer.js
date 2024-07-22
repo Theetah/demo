33344ee05c880b4154bb57642a97f21509387a8d
theetah <phantomporpoise@gmail.com>
2024-07-21 19:57:20 -0500

    new drawer collapse button, some other changes

---
+++ js/drawer.js
@@ -0,0 +1,13 @@
+// This script adds functionality to the "drawer" class ul and any related elements.
+
+
+// event for a button at the top of each drawer instance thing
+function collapseDrawer(event) {
+    const drawer = event.target.parentElement;
+    const listElements = drawer.querySelectorAll("input");
+
+    for (let i = 0; i < listElements.length; i++) {
+        const e = listElements[i];
+        e.checked = false;
+    }
+}
