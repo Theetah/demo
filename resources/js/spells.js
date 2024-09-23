d66940d7360c4d741b214df22b6e1078c244a852
theetah <phantomporpoise@gmail.com>
2024-09-23 17:25:17 -0500

    one little tiny change

--- resources/js/spells.js
+++ resources/js/spells.js
@@ -248,7 +248,7 @@ window.addEventListener("load", function() {
 
       main.innerHTML = 
       `
-        <a href="/spells.html">
+        <a href="/demo/spells.html">
           <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#e8eaed">
             <path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/>
           </svg>
